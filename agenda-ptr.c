95a9bb8e79fe84e5674c45993e7d94ea071dc620
LucasAlviene <lucasalp1@hotmail.com>
2019-09-27 14:53:55 -0300

    27-09

--- agenda-ptr.c
+++ agenda-ptr.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define PR 10
+#define PR 3
 void *getP(void *pBuffer,int nPos);
-void *addP(void *pBuffer);
+void imprimir(void *pBuffer);
 void add(void *pBuffer);
+
+struct info{
+    int id;
+    int telefone;
+    char nome[];
+};
+
+typedef struct info Pessoa;
 
 int main(){
     void *pBuffer = NULL;
     int *escolha,*tamanho,*qnt,*nPos,*n;
 
-    pBuffer = (void *)malloc(PR * sizeof(int));
+    pBuffer = (void *)malloc(80000);
     escolha = (int *)(pBuffer);
-    nPos = (int *)(pBuffer + sizeof(int));
-    qnt = (int *)(pBuffer + sizeof(int) * 2);
+    qnt = (int *)(pBuffer + sizeof(int));
+    nPos = (int *)(pBuffer + sizeof(int)*2);
     *qnt = 0;
     *escolha = 1;
+
     do{
         printf(" -- MENU --\n1 - Adicionar\n2 - Procurar\n3 - Remover\nEscolha um numero: ");
 
@@ -23,43 +32,48 @@ int main(){
         switch (*escolha)
         {
             case 1:
+                (*qnt)++;
+                //pBuffer = realloc(pBuffer,(sizeof(int) * PR + (sizeof(Pessoa) * (*qnt) * 5)));
+                escolha = (int *)(pBuffer);
+                qnt = (int *)(pBuffer + sizeof(int));
                 add(pBuffer);
             break;
             case 2:
+                imprimir(pBuffer);
             break;
         }
-        
+        printf("\n\n%d\n\n",*qnt);
     }while(*escolha != 0);
+}
 
+void imprimir(void *pBuffer){
+    int *i,*qnt;
+    Pessoa *pInicio,*pAtual;
+    
+    qnt = (int *)(pBuffer + sizeof(int));
+    i = (int *)(pBuffer + sizeof(int)*2);
+    *i = 0;
+    pInicio = (Pessoa *)(pBuffer + sizeof(int) * PR + sizeof(Pessoa));
+    while(*i<*qnt){
+        printf("Nome: %s\nTelefone:%d\n",pInicio->nome,pInicio->telefone);
+        pInicio += sizeof(Pessoa);;
+        (*i)++;
+    }
 }
 
 void add(void *pBuffer){
-    int *valor,*n,*numero;
+    int *qnt;
+    Pessoa *pAtual;
     
-    //valor = (int *)(pBuffer + sizeof(int) * PR + sizeof(int));
+    qnt = (int *)(pBuffer + sizeof(int));
 
-    valor = (int *)addP(pBuffer);
-    scanf("%d",valor);
-    
-
-    n = (int *)getP(pBuffer,1);
-    printf("\n%d\n",*n);
+    pAtual = (Pessoa *)(pBuffer + sizeof(int) * PR + sizeof(Pessoa) * (*qnt));
+    printf("Insira o nome: ");
+	scanf("%s", (*pAtual).nome);
+	printf("Insira a Telefone: ");
+	scanf("%d", &(*pAtual).telefone);
 }
 
-void *getP(void *pBuffer,int nPos){
-
-    return pBuffer + sizeof(int) * PR + sizeof(int) * 4;
-}
-
-void *addP(void *pBuffer){
-    int *qnt,*novo;
-    qnt = (int *)(pBuffer + sizeof(int) * 2);
-    *qnt += 1;
-    novo = (pBuffer + sizeof(int) * PR) + sizeof(int) * (*qnt);
-
-    //pBuffer = realloc(pBuffer,(pBuffer + sizeof(int) * PR) + sizeof(int) * (*qnt));
-    return novo;
-}
 
 // git config --global user.name ""
 // git config --global user.email ""
